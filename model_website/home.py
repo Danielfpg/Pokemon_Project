a2150c2529468dde5dbab7b342693ab63af40cda
Danielfpg <danielfelipehg10@gmail.com>
2025-06-02 21:14:12 -0500

    opciones de entrenador

--- model_website/home.py
+++ model_website/home.py
@@ -5,11 +5,17 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
 
+from Models.Model_pydantic.Model_Trainer_card import CartaEntrenador
+from Operations.Operations_db.db_Operations_Trainer_Card import crear_carta_entrenador
+from Models.Model_db.Model_Trainer_card_db import CartaEntrenadorDB
+
 from Models.Model_db.Model_Pokemon_card_db import CartaPokemonDB
 from Models.Model_pydantic.Model_stats import Stats
 from Operations.Operations_db.db_Operations_Pokemon_Card import crear_carta_pokemon
 from Models.Model_pydantic.Model_Pokemon_card import CartaPokemon
 from db.db_connection import get_session
+
+
 
 templates = Jinja2Templates(directory="model_website/templates")
 
@@ -20,12 +26,14 @@ router = APIRouter()
 @router.get("/", response_class=HTMLResponse)
 async def read_home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
-
+#--------------------
+#POKEMON
+#--------------------
 @router.get("/add_pokemon", response_class=HTMLResponse)
 async def mostrar_formulario_pokemon(request: Request):
     return templates.TemplateResponse("add_pokemon.html", {"request": request})
 
-@router.post("/cartas/crear")
+@router.post("/cartas/crear/pokemon")
 async def agregar_carta_pokemon_form(
     request: Request,
     id: int = Form(...),
@@ -87,4 +95,57 @@ async def ver_pokemones(
     return templates.TemplateResponse("ver_pokemones.html", {
         "request": request,
         "pokemones": pokemones
+    })
+#--------------------
+#ENTRENADOR
+#--------------------
+@router.get("/add_entrenador", response_class=HTMLResponse)
+async def mostrar_formulario_entrenador(request: Request):
+    return templates.TemplateResponse("add_entrenador.html", {"request": request})
+
+@router.post("/cartas/crear/entrenador")
+async def agregar_carta_entrenador_form(
+    request: Request,
+    id: int = Form(...),
+    nombre: str = Form(...),
+    tipo_carta: str = Form(...),
+    subtipo: str = Form(...),
+    efecto: str = Form(...),
+    tiempo: str = Form(...),
+    rare: str = Form(...),
+    costo_en_bolsa: float = Form(...),
+    db: AsyncSession = Depends(get_session)
+):
+    carta = CartaEntrenador(
+        id=id,
+        nombre=nombre,
+        tipo_carta=tipo_carta.lower(),
+        subtipo=subtipo,
+        efecto=efecto,
+        tiempo=tiempo,
+        rare=rare,
+        costo_en_bolsa=costo_en_bolsa
+    )
+
+    await crear_carta_entrenador(db, carta)
+
+    return RedirectResponse(url="/", status_code=303)
+
+@router.get("/ver_entrenador", response_class=HTMLResponse)
+async def ver_entrenador(
+    request: Request,
+    session: AsyncSession = Depends(get_session),
+    q: str = Query(default=None)
+):
+    stmt = select(CartaEntrenadorDB)
+
+    if q:
+        stmt = stmt.where(CartaEntrenadorDB.nombre.ilike(f"%{q}%"))
+
+    result = await session.execute(stmt)
+    entrenador = result.scalars().all()
+
+    return templates.TemplateResponse("ver_entrenador.html", {
+        "request": request,
+        "entrenadores": entrenador
     })
